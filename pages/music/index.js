5454e9d0fa0e5a810b3995ad982568d89267c967
Max Woollons <maxwoollons@outlook.com>
2023-03-20 10:40:01 +1000

    Add files via upload

--- pages/music/index.js
+++ pages/music/index.js
@@ -7,7 +7,7 @@ import 'remixicon/fonts/remixicon.css'
 import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 import { useState } from 'react'
-import Navbar from '../../components/navbar'
+import Navbar from '/components/navbar'
 
 
 
